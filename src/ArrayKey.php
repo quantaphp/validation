9d0a5b00e9c3de1a0192d4b1032e4ea20ac8a816
pmall <pierre.mallinjoud@gmail.com>
2022-09-15 17:06:32 +0200

    reflections...

--- src/ArrayKey.php
+++ src/ArrayKey.php
@@ -8,100 +8,82 @@ use Quanta\Validation\Rules;
 
 final class ArrayKey
 {
-    public static function required(string $key, ...$rules): self
+    public static function required(string $key): self
     {
         $keys = explode('.', $key);
 
         $head = array_shift($keys);
 
-        $instance = new self(Result::bind(new Rules\Required($head)));
+        $instance = new self(new Rules\Required($head));
 
         $reducer = fn ($instance, $key) => $instance
             ->with(new Rules\IsArray)
             ->with(new Rules\Required($key));
 
-        return array_reduce($keys, $reducer, $instance)->rule(...$rules);
+        return array_reduce($keys, $reducer, $instance);
     }
 
-    public static function optional(string $key, mixed $default = null, ...$rules): self
+    public static function optional(string $key, $default = null): self
     {
         $keys = explode('.', $key);
 
         $head = array_shift($keys);
 
-        $instance = new self(Result::bind(new Rules\Optional($head, $default)));
+        $instance = new self(new Rules\Optional($head, $default));
 
         $reducer = fn ($instance, $key) => $instance
             ->with(new Rules\IsArray)
             ->with(new Rules\Optional($key, $default));
 
-        return array_reduce($keys, $reducer, $instance)->rule(...$rules);
+        return array_reduce($keys, $reducer, $instance);
     }
+
+    public static function int(string $key, ?int ...$default): self
+    {
+        return self::instance($key, ...$default)->with(new Rules\IsInt);
+    }
+
+    public static function float(string $key, ?float ...$default): self
+    {
+        return self::instance($key, ...$default)->with(new Rules\IsFloat);
+    }
+
+    public static function string(string $key, ?string ...$default): self
+    {
+        return self::instance($key, ...$default)->with(new Rules\IsString);
+    }
+
+    public static function array(string $key, ?array ...$default): self
+    {
+        return self::instance($key, ...$default)->with(new Rules\IsArray);
+    }
+
+    private static function instance(string $key, ...$default): self
+    {
+        return count($default) == 0
+            ? self::required($key)
+            : self::optional($key, $default[0]);
+    }
+
+    /**
+     * @var callable(array): \Quanta\Validation\Result
+     */
+    private $head;
 
     /**
      * @var Array<callable(mixed): \Quanta\Validation\Result>
      */
     private array $rules;
 
-    private function __construct(callable ...$rules)
+    private function __construct(callable $head, callable ...$rules)
     {
+        $this->head = $head;
         $this->rules = $rules;
-    }
-
-    public function int(...$rules): self
-    {
-        return $this->with(new Rules\IsInt)->rule(...$rules);
-    }
-
-    public function float(...$rules): self
-    {
-        return $this->with(new Rules\IsFloat)->rule(...$rules);
-    }
-
-    public function string(...$rules): self
-    {
-        return $this->with(new Rules\IsString)->rule(...$rules);
-    }
-
-    public function array(...$rules): self
-    {
-        return $this->with(new Rules\IsArray)->rule(...$rules);
-    }
-
-    public function factory(callable $f): self
-    {
-        return $this->with(new Rules\Wrapper($f));
-    }
-
-    /**
-     * @param string|callable(mixed): \Quanta\Validation\Result ...$rules
-     */
-    public function rule(...$rules): self
-    {
-        if (count($rules) == 0) return $this;
-
-        $rule = array_shift($rules);
-
-        if (is_string($rule)) {
-            if (is_callable([$rule, 'from'])) {
-                return $this->factory([$rule, 'from'])->rule(...$rules);
-            }
-
-            throw new \InvalidArgumentException(sprintf('[%s, \'from\'] is not a callable', $rule));
-        }
-
-        if (is_callable($rule)) {
-            return $this->with($rule)->rule(...$rules);
-        }
-
-        throw new \InvalidArgumentException(
-            'Rule must be either a callable or the name of a class with a static \'from\' method',
-        );
     }
 
     private function with(callable $rule): self
     {
-        return new self(...$this->rules, ...[Result::bind($rule)]);
+        return new self($this->head, ...$this->rules, ...[Result::bind($rule)]);
     }
 
     /**
@@ -109,7 +91,7 @@ final class ArrayKey
      */
     public function __invoke(array $data): Result
     {
-        $init = Result::unit($data);
+        $init = ($this->head)($data);
         $reducer = fn ($result, $rule) => $rule($result);
 
         return array_reduce($this->rules, $reducer, $init);
